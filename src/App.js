a995788c184c18065bbe6e684524fa2773aa1efa
Jae Wook Jung <86764469+armario99@users.noreply.github.com>
2024-06-17 15:17:08 +0900

    docs: 페이지 css 일부 수정(외각선 -> 그림자)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import './App.css';
-//import './components/Button'
+import { Routes, Route } from "react-router-dom";
+import React, { useState} from'react';
+
+import LoginPage from "./pages/Login/Login/Login";
+import JoinPage from "./pages/Login/Join/Join";
+
 function App() {
   return (
     <div className="App">
-      
+      <Routes> 
+          <Route path="/Login" element={<LoginPage/>} />
+          <Route path="/Join" element={<JoinPage/>} />
+      </Routes>
     </div>
   );
 }
