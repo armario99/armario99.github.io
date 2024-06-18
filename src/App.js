ae9f9089132e11ce2e698ecc71fa2cb5731b2822
Jae Wook Jung <86764469+armario99@users.noreply.github.com>
2024-06-18 14:24:24 +0900

    feat:마이페이지 임시 추가

--- src/App.js
+++ src/App.js
@@ -3,13 +3,18 @@ import React, { useState} from'react';
 
 import LoginPage from "./pages/Login/Login/Login";
 import JoinPage from "./pages/Login/Join/Join";
-
+import MyPage from "./pages/Mypage/MyPage/Mypage";
+import InformPage from "./pages/Mypage/InformPage/inform";
 function App() {
+  const [selectedImage, setSelectedImage] = useState(null);
+  
   return (
     <div className="App">
       <Routes> 
           <Route path="/Login" element={<LoginPage/>} />
           <Route path="/Join" element={<JoinPage/>} />
+          <Route path="/MyPage" element={<MyPage selectedImage ={selectedImage} setSelectedImage= {setSelectedImage} />} />
+          <Route path="/InformPage" element={<InformPage selectedImage ={selectedImage} setSelectedImage= {setSelectedImage} />} />
       </Routes>
     </div>
   );
